7db7700931c78abeefeebe2c12cd4ff761bbbd48
nandoorath001 <nandoorath001@nandoorath00170.us.nam.ad.pwcinternal.com>
2018-03-02 18:55:57 +0530

    code2

--- Department/src/test/java/section/sectionATest.java
+++ Department/src/test/java/section/sectionATest.java
@@ -6,12 +6,16 @@ public class sectionATest {
 	
 	@Test
 	public void astart(){
-		System.out.println("Section A Starts");
+		System.out.println("Section AGit Starts");
 	}
 	
 	@Test
 	public void astop(){
-		System.out.println("Section A Stops");
+		System.out.println("Section AGit Stops");
+	}
+	@Test
+	public void agit(){
+		System.out.println("GIT");
 	}
 
 }
